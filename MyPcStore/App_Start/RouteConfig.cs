4a464064cec1b7c2fb206ab76f70bd8a3046525c
therafk <therafk@hotmail.com>
2018-08-04 10:25:16 +0100

    main Controllers folder , created new controller Pages and View

--- MyPcStore/App_Start/RouteConfig.cs
+++ MyPcStore/App_Start/RouteConfig.cs
@@ -13,11 +13,28 @@ namespace MyPcStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute("Account", "Account/{action}/{id}", new { controller = "Account", action = "Index", id = UrlParameter.Optional }, new[] { "MyPcStore.Controllers" });
+
+            //routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "MyPcStore.Controllers" });
+
+            //routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "MyPcStore.Controllers" });
+
+            //routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "MyPcStore.Controllers" });
+
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "MyPcStore.Controllers" });
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "MyPcStore.Controllers" });
+            
+            //rout is an empty string               heres defaults - default for the home page
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "MyPcStore.Controllers" });
+
+
+
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ MyPcStore/Controllers/PagesController.cs
@@ -0,0 +1,73 @@
+﻿using MyPcStore.Models.Data;
+using MyPcStore.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPcStore.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{page} it's optional page
+        public ActionResult Index(string page = "") //optional page parameter
+        {
+            
+            if (page == "")     // Get/set page slug
+                page = "home";  //this page is the slug
+
+            
+            PageVM myModel;     // Declare model and View Model
+            PageDTO dto;        // Declare model and DTO
+
+            // Check if page exists
+            using (Db db = new Db())
+            {
+                if (!db.Pages.Any(y => y.Slug.Equals(page)))   //if no match its a problem
+                {
+                    return RedirectToAction("Index", new { page = "" }); //rederect to home page
+                }
+            }
+
+            // get page data transfer object
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(y => y.Slug == page).FirstOrDefault();
+            }                           //FirstOrDefault() if there is no match it returns null
+
+            
+            ViewBag.PageTitle = dto.Title;  // set the page title
+
+            
+            if (dto.HasSidebar == true)     // check if there's sidebar
+            {
+                ViewBag.Sidebar = "Yes";        //sidebar is bollean 
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+
+            myModel = new PageVM(dto);    // initialize model
+            return View(myModel);         // return view with model
+        }
+
+
+        public ActionResult PagesMenuPartial()
+        {
+            
+            List<PageVM> pagesVMList; // Declare a list of PageVM
+
+            using (Db db = new Db())// get all pages except the home
+            {                       //ascending order is default
+                pagesVMList = db.Pages.ToArray().OrderBy(y => y.Sorting).Where(y => y.Slug != "home").Select(y => new PageVM(y)).ToList();
+            }
+            
+            return PartialView(pagesVMList); // return partial view with the list
+        }
+
+
+
+    }
+}
