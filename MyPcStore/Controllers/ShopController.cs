4b32025427bbd130461a15c16995a8b517029e04
therafk <therafk@hotmail.com>
2018-08-05 09:33:18 +0100

    method menu

---
+++ MyPcStore/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using MyPcStore.Models.Data;
+using MyPcStore.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPcStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Pages"); //index method/pages controller
+        }
+
+        public ActionResult CategoryMenuPartial()
+        {
+            
+            List<CategoryVM> catVMList; // declare list CategoryVM
+
+            
+            using (Db db = new Db()) // initialize the list
+            {
+                catVMList = db.Categories.ToArray().OrderBy(y => y.Sorting).Select(y => new CategoryVM(y)).ToList();
+            }
+            
+            return PartialView(catVMList);// return partial with list
+        }
+    }
+}
