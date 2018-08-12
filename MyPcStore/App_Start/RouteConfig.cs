cd6a19f2700672d2f2d97bbba60caeaebc54f513
therafk <therafk@hotmail.com>
2018-08-12 09:32:10 +0100

    Cart controller; viemodel; css for partial view

--- MyPcStore/App_Start/RouteConfig.cs
+++ MyPcStore/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace MyPcStore
 
             //routes.MapRoute("Account", "Account/{action}/{id}", new { controller = "Account", action = "Index", id = UrlParameter.Optional }, new[] { "MyPcStore.Controllers" });
 
-            //routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "MyPcStore.Controllers" });
+            routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "MyPcStore.Controllers" });
 
             routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "MyPcStore.Controllers" });
 
---
+++ MyPcStore/Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using MyPcStore.Models.ViewModels.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPcStore.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CartPartial()
+        {
+            CartVM myModel = new CartVM();    // Initialize CartVM
+            
+            int qty = 0;          // initial quantity
+            
+            decimal price = 0m;     // initial price
+
+            if (Session["cart"] != null)    // check for cart session
+            {
+                var list = (List<CartVM>)Session["cart"];   // get total qty and price
+
+                foreach (var item in list)
+                {
+                    qty += item.Quantity;
+                    price += item.Quantity * item.Price;
+                }
+
+                myModel.Quantity = qty;
+                myModel.Price = price;
+            }
+            else
+            {
+                myModel.Quantity = 0;   // or set qty and price to 0
+                myModel.Price = 0m;
+            }
+            return PartialView(myModel);
+        }
+    }
+}
