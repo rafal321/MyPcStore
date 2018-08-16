b5afa7c00c0dba0edea41c5a56bc97e3379c70e2
therafk <therafk@hotmail.com>
2018-08-16 10:09:07 +0100

    SetUp Roles 1)Admin 2)Users

--- MyPcStore/Models/Data/Db.cs
+++ MyPcStore/Models/Data/Db.cs
@@ -15,6 +15,7 @@ namespace MyPcStore.Models.Data
         public DbSet<CategoryDTO> Categories { get; set; }
         public DbSet<ProductDTO> Products { get; set; }
         public DbSet<UserDTO> Users { get; set; }
+        public DbSet<RoleDTO> Roles { get; set; }
 
     }
 }
