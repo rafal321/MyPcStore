67756768d4748275f1270b2f4adab3ed993dff60
therafk <therafk@hotmail.com>
2018-08-16 12:18:51 +0100

    Creating first user + validation adding admin user manually to a table + fixing tables

--- MyPcStore/Controllers/AccountController.cs
+++ MyPcStore/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyPcStore.Models.Data;
+using MyPcStore.Models.ViewModels.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,13 +28,74 @@ namespace MyPcStore.Controllers
         }
 
 
-        // get: /account/create-account
+        // get: /account/create-account ---------------------------
         [ActionName("create-account")]
         [HttpGet]
         public ActionResult CreateAccount()
         {
             return View("CreateAccount");
         }
+
+        // post: /account/create-account ---------------------------
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserVM myModel)
+        {
+            
+            if (!ModelState.IsValid)                // check model state
+            {
+                return View("CreateAccount", myModel);
+            }
+
+            // check for passwords match
+            if (!myModel.Password.Equals(myModel.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Password do not match.");
+                return View("CreateAccount", myModel);
+            }
+
+            using (Db db = new Db())
+            {
+                // username has to be unique
+                if (db.Users.Any(y => y.Username.Equals(myModel.Username)))
+                {
+                    ModelState.AddModelError("", "Username " + myModel.Username + " is taken.");
+                    myModel.Username = "";
+                    return View("CreateAccount", myModel);
+                }
+                
+                UserDTO userDTO = new UserDTO() // create userDTO
+                {
+                    FirstName = myModel.FirstName,
+                    LastName = myModel.LastName,
+                    EmailAddress = myModel.EmailAddress,
+                    Username = myModel.Username,
+                    Password = myModel.Password
+                };
+                
+                db.Users.Add(userDTO);  // add the DTO
+                
+                db.SaveChanges();       // save
+
+                
+                int id = userDTO.Id;    // add to UserRolesDTO
+
+                UserRoleDTO userRoleDTO = new UserRoleDTO()
+                {
+                    UserId = id, //initialize fields
+                    RoleId = 2  //this is
+                };
+
+                db.UserRoles.Add(userRoleDTO);
+                db.SaveChanges();
+            }
+
+            TempData["SuccessMessage"] = "You are registered. You can login now."; // Create a TempData message
+
+            // Redirect
+            return Redirect("~/account/login");
+        }
+
 
     }
 }
