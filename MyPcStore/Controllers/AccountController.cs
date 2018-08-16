abcd1a40b8254643e05a6fb4b50791ad23c21024
therafk <therafk@hotmail.com>
2018-08-16 11:39:42 +0100

    Login + errors fixed

--- MyPcStore/Controllers/AccountController.cs
+++ MyPcStore/Controllers/AccountController.cs
@@ -11,8 +11,20 @@ namespace MyPcStore.Controllers
         // GET: Account
         public ActionResult Index()
         {
-            return View();
+            return Redirect("~/account/login");
         }
+        
+        [HttpGet]
+        public ActionResult Login()             // GET: /account/login
+        {
+            string userName = User.Identity.Name;   // make sure user is not logged in
+                                                    //Identity created by system
+            if (!string.IsNullOrEmpty(userName))
+                return RedirectToAction("user-profile");
+            
+            return View(); // return view
+        }
+
 
         // get: /account/create-account
         [ActionName("create-account")]
