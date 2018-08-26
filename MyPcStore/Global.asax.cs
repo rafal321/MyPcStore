a82ba8dc0b82de84c3ff75c90c46ed3bb8bba403
therafk <therafk@hotmail.com>
2018-08-26 11:10:05 +0100

    authentication

--- MyPcStore/Global.asax.cs
+++ MyPcStore/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MyPcStore.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,25 @@ namespace MyPcStore
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+            if (User == null) { return; } // veryfi if user is logged in
+            string user_name = Context.User.Identity.Name; //  username
+
+            // Declare array of roles
+            string[] roles = null;
+
+            using (Db db = new Db())
+            {
+                UserDTO dto = db.Users.FirstOrDefault(y => y.Username == user_name);
+                roles = db.UserRoles.Where(y => y.UserId == dto.Id).Select(y => y.Role.Name);
+            }
+
+            IIdentity user_Identity = new GenericIdentity(user_name);
+            IPrincipal new_UserObj = new GenericPrincipal(user_Identity, roles);
+            Context.User = new_UserObj;     // updating Context.User
         }
     }
 }
