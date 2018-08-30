b9db7aa395e0ac64596499ac31ce14a0eec6d5dd
therafk <therafk@hotmail.com>
2018-08-30 10:36:03 +0100

    Ability for admin to view all orders Pt.1

---
+++ MyPcStore/Models/ViewModels/Shop/OrderVM.cs
@@ -0,0 +1,26 @@
+﻿using MyPcStore.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyPcStore.Models.ViewModels.Shop
+{
+    public class OrderVM
+    {
+        public OrderVM()
+        {
+        }
+
+        public OrderVM(OrderDTO rows)
+        {
+            OrderId = rows.OrderId;
+            UserId = rows.UserId;
+            CreatedAt = rows.CreatedAt;
+        }
+
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
