fe5ed37b78ae6d3bb07a67eb8fcc6df6e5fec76e
therafk <therafk@hotmail.com>
2018-10-15 17:52:12 +0100

    ***Test SetUp*** public interface TestTDbSet : IDisposable

---
+++ MyPcStore/Models/Abstract/TestTDbSet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyPcStore.Models.Data;
+using System.Data.Entity;
+
+namespace MyPcStore.Models.Abstract
+{
+    public interface TestTDbSet : IDisposable
+    {
+
+        DbSet<PageDTO> Pages { get; }
+        DbSet<SidebarDTO> Sidebar { get; }
+        DbSet<CategoryDTO> Categories { get; }
+        DbSet<ProductDTO> Products { get; }
+        DbSet<UserDTO> Users { get; }
+        DbSet<RoleDTO> Roles { get; }
+        DbSet<UserRoleDTO> UserRoles { get; }
+        DbSet<OrderDTO> Orders { get; }
+        DbSet<OrderDetailsDTO> OrderDetails { get; }
+
+        int SaveChanges();
+        void MarkAsModified(Object item);
+
+    }
+}
